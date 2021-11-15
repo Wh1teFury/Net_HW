14a11403f5c44931f1769c3eaee0efda3aa604c1
Roman Udalov <Udalovrom52@gmail.com>
2021-11-15 19:50:58 +0300

    homework_9

--- homework_9/event.js
+++ homework_9/event.js
@@ -4,25 +4,46 @@ const sortBtn = document.getElementById("sort-btn");
 const sortMenu = document.getElementById("sort-menu");
 const taskBtn = document.getElementById("task-add");
 const taskForm = document.getElementById("form-block");
-
-let timeCheck = '';
+const videoBtn = document.getElementById("show-video");
+const videoDiv = document.getElementById("video-wrap");
 
 sortBtn.addEventListener('click', () => changeBlock(sortMenu));
-bodyPage.addEventListener('click', () => removeBlock(sortMenu));
+bodyPage.addEventListener('click', (el) =>  {
+    let target = el.target;
+    removeBlock(sortMenu, target)
+});
+
 taskBtn.addEventListener('click', () => changeBlock(taskForm));
-bodyPage.addEventListener('click', () => removeBlock(taskForm));
+bodyPage.addEventListener('click', (el) => {
+    let target = el.target;
+    removeBlock (taskForm, target)
+});
+
+videoBtn.addEventListener('click', () => changeBlock(videoDiv));
+bodyPage.addEventListener('click', (el) => {
+    let target = el.target;
+    removeBlock (videoDiv, target)
+});
+
 
 function changeBlock (element) {
-    if (element.style.display == "block") {
-        element.style = "display: none";
-    } else {
-        element.style = "display: block"; timeCheck = 1;
-    }
+    setTimeout(function() {
+        element.classList.remove("hide");
+    }, 100);    
 }
 
-function removeBlock (element) {
-    if (!timeCheck) {
-        element.style = "display: none";
+function removeBlock (item, target) {
+    let emptyArray = [];
+    const nodeList = item.querySelectorAll("*")
+    for (element of nodeList) {
+        emptyArray.push((target === element))
     }
-    if (timeCheck) { setTimeout(function () { timeCheck = ''; }, 100); }
+    let listArray = [...new Set(emptyArray)];
+    if (listArray[1] == null) {
+        item.classList.add("hide");
+    } 
 }
+
+document.addEventListener('click', (e) => {
+    console.log(e.target)
+})
